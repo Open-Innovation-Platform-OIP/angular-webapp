446b0f81df15076d1c4e2c94ac9ffbd0a488de7b
vishnu venu <vishnuvenuvenk@gmail.com>
2019-03-22 11:37:18 +0530

    wizard component created

--- src/app/problems/wizard-container/wizard-container.component.ts
+++ src/app/problems/wizard-container/wizard-container.component.ts
@@ -1,5 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  OnDestroy,
+  AfterViewInit,
+  SimpleChanges
+} from "@angular/core";
 import { ErrorStateMatcher } from "@angular/material/core";
+import { TagsService } from "../../services/tags.service";
+import { FilesService } from "../../services/files.service";
+import { UsersService } from "../../services/users.service";
+import { AuthService } from "../../services/auth.service";
+import { Router, ActivatedRoute } from "@angular/router";
 
 import {
   FormControl,
@@ -9,6 +22,20 @@ import {
   Validators,
   FormGroup
 } from "@angular/forms";
+import { Content } from "@angular/compiler/src/render3/r3_ast";
+declare const $: any;
+
+let canProceed: boolean;
+
+interface FileReaderEventTarget extends EventTarget {
+  result: string;
+}
+
+interface FileReaderEvent extends Event {
+  target: EventTarget;
+  getMessage(): string;
+}
+
 // import { Content } from '@angular/compiler/src/render3/r3_ast';
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -26,22 +53,25 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 }
 
 @Component({
-  selector: 'app-wizard-container',
-  templateUrl: './wizard-container.component.html',
-  styleUrls: ['./wizard-container.component.css']
+  selector: "app-wizard-container",
+  templateUrl: "./wizard-container.component.html",
+  styleUrls: ["./wizard-container.component.css"]
 })
-export class WizardContainerComponent implements OnInit {
+export class WizardContainerComponent
+  implements OnInit, OnChanges, AfterViewInit {
   @Input() content;
+  matcher = new MyErrorStateMatcher();
+  type: FormGroup;
 
   constructor(
+    private router: Router,
+    private route: ActivatedRoute,
     private formBuilder: FormBuilder,
-
-  ) { }
-
-  ngOnInit() {
-    console.log("checking the content: ", this.content);
-
-  }
+    private filesService: FilesService,
+    private tagService: TagsService,
+    private usersService: UsersService,
+    private auth: AuthService
+  ) {}
 
   displayFieldCss(form: FormGroup, field: string) {
     return {
@@ -52,5 +82,430 @@ export class WizardContainerComponent implements OnInit {
 
   isFieldValid(form: FormGroup, field: string) {
     return !form.get(field).valid && form.get(field).touched;
+  }
+
+  ngOnInit() {
+    canProceed = true;
+    // clearInterval(this.autosaveInterval);
+    // this.autosaveInterval = setInterval(() => {
+    //   this.autoSave();
+    // }, 10000);
+    // this.route.params.pipe(first()).subscribe(params => {
+    //   if (params.id) {
+    //     this.apollo
+    //       .watchQuery<any>({
+    //         query: gql`
+    //                     {
+    //                         problems(where: { id: { _eq: ${params.id} } }) {
+    //                         id
+    //                         title
+    //                         created_by
+    //                         description
+    //                         location
+    //                         resources_needed
+    //                         image_urls
+    //                         video_urls
+    //                         impact
+    //                         extent
+    //                         min_population
+    //                         beneficiary_attributes
+    //                         organization
+    //                         featured_url
+    //                         featured_type
+    //                         voted_by
+    //                         watched_by
+    //                         problem_tags{
+    //                             tag {
+    //                                 id
+    //                                 name
+    //                             }
+    //                         }
+    //                         }
+    //                     }
+    //                     `,
+    //         pollInterval: 500
+    //       })
+    //       .valueChanges.subscribe(result => {
+    //         if (
+    //           result.data.problems.length >= 1 &&
+    //           result.data.problems[0].id
+    //         ) {
+    //           canProceed = true;
+    //           this.problem["id"] = result.data.problems[0].id;
+    //           Object.keys(this.problem).map(key => {
+    //             // console.log(key, result.data.problems[0][key]);
+    //             if (result.data.problems[0][key]) {
+    //               this.problem[key] = result.data.problems[0][key];
+    //             }
+    //           });
+    //           if (this.problem.title) {
+    //             this.smartSearch(this.problem.title);
+    //           }
+    //           this.sectors = result.data.problems[0].problem_tags.map(
+    //             tagArray => {
+    //               return tagArray.tag.name;
+    //             }
+    //           );
+    //           this.is_edit = true;
+    //         } else {
+    //           this.router.navigate(["problems/add"]);
+    //         }
+    //       });
+    //   }
+    // });
+    // this.tagService.getTagsFromDB();
+    // this.usersService.getOrgsFromDB();
+    this.type = this.formBuilder.group({
+      // To add a validator, we must first convert the string value into an array.
+      // The first tag in the array is the default value if any,
+      // then the next tag in the array is the validator.
+      // Here we are adding a required validator meaning that the firstName attribute must have a value in it.
+      title: [null, Validators.required],
+      description: [null, Validators.required],
+      location: [null, Validators.required],
+      population: [null, Validators.required],
+      organization: [null, Validators.required],
+      impact: [null, null],
+      extent: [null, null],
+      beneficiary: [null, null],
+      resources: [null, null],
+      sectors: [null, null],
+      media_url: [null, null]
+    });
+    // Code for the Validator
+    const $validator = $(".card-wizard form").validate({
+      rules: {
+        title: {
+          required: true,
+          minlength: 3
+        },
+        description: {
+          required: true
+        },
+        organization: {
+          required: true
+        },
+        location: {
+          required: true
+        },
+        population: {
+          required: true
+        }
+      },
+
+      highlight: function(element) {
+        $(element)
+          .closest(".form-group")
+          .removeClass("has-success")
+          .addClass("has-danger");
+      },
+      success: function(element) {
+        $(element)
+          .closest(".form-group")
+          .removeClass("has-danger")
+          .addClass("has-success");
+      },
+      errorPlacement: function(error, element) {
+        $(element).append(error);
+      }
+    });
+
+    // Wizard Initialization
+    $(".card-wizard").bootstrapWizard({
+      tabClass: "nav nav-pills",
+      nextSelector: ".btn-next",
+      previousSelector: ".btn-previous",
+
+      onNext: function(tab, navigation, index) {
+        const $valid = $(".card-wizard form").valid();
+        if (!$valid) {
+          $validator.focusInvalid();
+          return false;
+        } else if (!canProceed) {
+          if (
+            confirm(
+              "Are you sure you want to add a new problem and not enrich an existing one?"
+            )
+          ) {
+            canProceed = true;
+            return true;
+          } else {
+            return false;
+          }
+        }
+      },
+
+      onInit: function(tab: any, navigation: any, index: any) {
+        // check number of tabs and fill the entire row
+        let $total = navigation.find("li").length;
+        const $wizard = navigation.closest(".card-wizard");
+
+        const $first_li = navigation.find("li:first-child a").html();
+        const $moving_div = $(
+          '<div class="moving-tab">' + $first_li + "</div>"
+        );
+        $(".card-wizard .wizard-navigation").append($moving_div);
+
+        $total = $wizard.find(".nav li").length;
+        let $li_width = 100 / $total;
+
+        const total_steps = $wizard.find(".nav li").length;
+        let move_distance = $wizard.width() / total_steps;
+        let index_temp = index;
+        let vertical_level = 0;
+
+        const mobile_device = $(document).width() < 600 && $total > 3;
+
+        if (mobile_device) {
+          move_distance = $wizard.width() / 2;
+          index_temp = index % 2;
+          $li_width = 50;
+        }
+
+        $wizard.find(".nav li").css("width", $li_width + "%");
+
+        const step_width = move_distance;
+        move_distance = move_distance * index_temp;
+
+        const $current = index + 1;
+
+        if ($current === 1 || (mobile_device === true && index % 2 === 0)) {
+          move_distance -= 8;
+        } else if (
+          $current === total_steps ||
+          (mobile_device === true && index % 2 === 1)
+        ) {
+          move_distance += 8;
+        }
+
+        if (mobile_device) {
+          const x: any = index / 2;
+          vertical_level = parseInt(x, 10);
+          vertical_level = vertical_level * 38;
+        }
+
+        $wizard.find(".moving-tab").css("width", step_width);
+        $(".moving-tab").css({
+          transform:
+            "translate3d(" + move_distance + "px, " + vertical_level + "px, 0)",
+          transition: "all 0.5s cubic-bezier(0.29, 1.42, 0.79, 1)"
+        });
+        $(".moving-tab").css("transition", "transform 0s");
+      },
+
+      onTabClick: function(tab: any, navigation: any, index: any) {
+        return true;
+        const $valid = $(".card-wizard form").valid();
+
+        if (!$valid) {
+          return false;
+        } else {
+          return true;
+        }
+      },
+
+      onTabShow: function(tab: any, navigation: any, index: any) {
+        let $total = navigation.find("li").length;
+        let $current = index + 1;
+
+        const $wizard = navigation.closest(".card-wizard");
+
+        // If it's the last tab then hide the last button and show the finish instead
+        if ($current >= $total) {
+          $($wizard)
+            .find(".btn-next")
+            .hide();
+          $($wizard)
+            .find(".btn-finish")
+            .show();
+        } else {
+          $($wizard)
+            .find(".btn-next")
+            .show();
+          $($wizard)
+            .find(".btn-finish")
+            .hide();
+        }
+
+        const button_text = navigation
+          .find("li:nth-child(" + $current + ") a")
+          .html();
+
+        setTimeout(function() {
+          $(".moving-tab").text(button_text);
+        }, 150);
+
+        const checkbox = $(".footer-checkbox");
+
+        if (index !== 0) {
+          $(checkbox).css({
+            opacity: "0",
+            visibility: "hidden",
+            position: "absolute"
+          });
+        } else {
+          $(checkbox).css({
+            opacity: "1",
+            visibility: "visible"
+          });
+        }
+        $total = $wizard.find(".nav li").length;
+        let $li_width = 100 / $total;
+
+        const total_steps = $wizard.find(".nav li").length;
+        let move_distance = $wizard.width() / total_steps;
+        let index_temp = index;
+        let vertical_level = 0;
+
+        const mobile_device = $(document).width() < 600 && $total > 3;
+
+        if (mobile_device) {
+          move_distance = $wizard.width() / 2;
+          index_temp = index % 2;
+          $li_width = 50;
+        }
+
+        $wizard.find(".nav li").css("width", $li_width + "%");
+
+        const step_width = move_distance;
+        move_distance = move_distance * index_temp;
+
+        $current = index + 1;
+
+        if ($current === 1 || (mobile_device === true && index % 2 === 0)) {
+          move_distance -= 8;
+        } else if (
+          $current === total_steps ||
+          (mobile_device === true && index % 2 === 1)
+        ) {
+          move_distance += 8;
+        }
+
+        if (mobile_device) {
+          const x: any = index / 2;
+          vertical_level = parseInt(x, 10);
+          vertical_level = vertical_level * 38;
+        }
+
+        $wizard.find(".moving-tab").css("width", step_width);
+        $(".moving-tab").css({
+          transform:
+            "translate3d(" + move_distance + "px, " + vertical_level + "px, 0)",
+          transition: "all 0.5s cubic-bezier(0.29, 1.42, 0.79, 1)"
+        });
+      }
+    });
+
+    // Prepare the preview for profile picture
+    $("#wizard-picture").change(function() {
+      const input = $(this);
+
+      if (input[0].files && input[0].files[0]) {
+        const reader = new FileReader();
+
+        reader.onload = function(e: any) {
+          $("#wizardPicturePreview")
+            .attr("src", e.target.result)
+            .fadeIn("slow");
+        };
+        reader.readAsDataURL(input[0].files[0]);
+      }
+    });
+
+    $('[data-toggle="wizard-radio"]').click(function() {
+      const wizard = $(this).closest(".card-wizard");
+      wizard.find('[data-toggle="wizard-radio"]').removeClass("active");
+      $(this).addClass("active");
+      $(wizard)
+        .find('[type="radio"]')
+        .removeAttr("checked");
+      $(this)
+        .find('[type="radio"]')
+        .attr("checked", "true");
+    });
+
+    $('[data-toggle="wizard-checkbox"]').click(function() {
+      if ($(this).hasClass("active")) {
+        $(this).removeClass("active");
+        $(this)
+          .find('[type="checkbox"]')
+          .removeAttr("checked");
+      } else {
+        $(this).addClass("active");
+        $(this)
+          .find('[type="checkbox"]')
+          .attr("checked", "true");
+      }
+    });
+    $(".set-full-height").css("height", "auto");
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    const input = $(this);
+    if (input[0].files && input[0].files[0]) {
+      const reader: any = new FileReader();
+      reader.onload = function(e: any) {
+        $("#wizardPicturePreview")
+          .attr("src", e.target.result)
+          .fadeIn("slow");
+      };
+      reader.readAsDataURL(input[0].files[0]);
+    }
+  }
+  ngAfterViewInit() {
+    $(window).resize(() => {
+      $(".card-wizard").each(function() {
+        const $wizard = $(this);
+        const index = $wizard.bootstrapWizard("currentIndex");
+        const $total = $wizard.find(".nav li").length;
+        let $li_width = 100 / $total;
+
+        const total_steps = $wizard.find(".nav li").length;
+        let move_distance = $wizard.width() / total_steps;
+        let index_temp = index;
+        let vertical_level = 0;
+
+        const mobile_device = $(document).width() < 600 && $total > 3;
+
+        if (mobile_device) {
+          move_distance = $wizard.width() / 2;
+          index_temp = index % 2;
+          $li_width = 50;
+        }
+
+        $wizard.find(".nav li").css("width", $li_width + "%");
+
+        const step_width = move_distance;
+        move_distance = move_distance * index_temp;
+
+        const $current = index + 1;
+
+        if ($current === 1 || (mobile_device === true && index % 2 === 0)) {
+          move_distance -= 8;
+        } else if (
+          $current === total_steps ||
+          (mobile_device === true && index % 2 === 1)
+        ) {
+          move_distance += 8;
+        }
+
+        if (mobile_device) {
+          const x: any = index / 2;
+          vertical_level = parseInt(x, 10);
+          vertical_level = vertical_level * 38;
+        }
+
+        $wizard.find(".moving-tab").css("width", step_width);
+        $(".moving-tab").css({
+          transform:
+            "translate3d(" + move_distance + "px, " + vertical_level + "px, 0)",
+          transition: "all 0.5s cubic-bezier(0.29, 1.42, 0.79, 1)"
+        });
+
+        $(".moving-tab").css({
+          transition: "transform 0s"
+        });
+      });
+    });
   }
 }
