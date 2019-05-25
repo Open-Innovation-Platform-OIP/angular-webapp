bd26ae1b7c0a320517f62b72b31d733ee2313df4
Kalpitha <kalpitha20@gmail.com>
2019-05-25 18:36:38 +0530

    added back to top button

--- src/app/layouts/admin/admin-layout.component.ts
+++ src/app/layouts/admin/admin-layout.component.ts
@@ -182,4 +182,19 @@ export class AdminLayoutComponent implements OnInit, AfterViewInit {
     }
     return bool;
   }
+
+  showButton() {
+    if (
+      document.body.scrollTop > 40 ||
+      document.documentElement.scrollTop > 40
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
 }
