d659c1e65f59c5de850fedf7a2c9e29f26ca01a1
vipin saini <vipin@jaaga.in>
2019-10-13 16:40:28 +0530

    modified the accordion and add some aria labels

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -262,12 +262,14 @@ export class LoginComponent implements OnInit, OnDestroy {
         },
         template:
           '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0} alert-with-icon" role="alert">' +
-          '<button mat-raised-button type="button" aria-hidden="true" class="close" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
+          '<button mat-raised-button type="button" aria-hidden="true" class="close" data-notify="dismiss"> ' +
+          '<i class="material-icons">close</i></button>' +
           `<i class="material-icons" data-notify="icon">${icon}</i>` +
           '<span data-notify="title">{1}</span> ' +
           '<span data-notify="message">{2}</span>' +
           '<div class="progress" data-notify="progressbar">' +
-          '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+          '<div class="progress-bar progress-bar-{0}" role="progressbar" ' +
+          'aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
           '</div>' +
           '<a href="{3}" target="{4}" data-notify="url"></a>' +
           '</div>'
