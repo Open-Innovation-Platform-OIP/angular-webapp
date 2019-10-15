6d3d66b860ca7392ac88fb593f54f0b5ee8b5ef4
vishnu venu <vishnuvenuvenk@gmail.com>
2019-10-15 17:30:54 +0530

    approval feature working

--- src/app/admin-view/admin-view.component.ts
+++ src/app/admin-view/admin-view.component.ts
@@ -7,6 +7,7 @@ import { AuthService } from "../services/auth.service";
 import swal from "sweetalert2";
 import { Subscription } from "rxjs";
 import { HttpClient } from "@angular/common/http";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-admin-view",
@@ -15,11 +16,21 @@ import { HttpClient } from "@angular/common/http";
 })
 export class AdminViewComponent implements OnInit, OnDestroy {
   public userDataTable: TableData;
-  allUsers = {};
+  public unapprovedUserDataTable: TableData;
+
+  objectKeys = Object.keys;
+
+  allApprovedUsers = {};
+  allUnapprovedUsers = {};
+
   inviteeEmail: String = "";
 
   usersQuery: QueryRef<any>;
+  unapprovedUsersQuery: QueryRef<any>;
   usersSubscription: Subscription;
+  unapprovedUsersSubscription: Subscription;
+
+  userInviteForm: FormGroup;
 
   constructor(
     private apollo: Apollo,
@@ -29,6 +40,10 @@ export class AdminViewComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.getUsersFromDB();
+    this.getUnapprovedUsersFromDB();
+    this.userInviteForm = new FormGroup({
+      email: new FormControl("", [Validators.email])
+    });
   }
 
   generateUserTable(userData) {
@@ -49,16 +64,83 @@ export class AdminViewComponent implements OnInit, OnDestroy {
     };
   }
 
-  getUnapprovedUsersFromDB() {}
+  generateUnapprovedUserTable(userData) {
+    const userHeaderRow = ["Email", "Approve"];
+    let userDataRow = [];
+    Object.values(userData).map(user => {
+      // console.log(user, "gnerate user table");
+      userDataRow.push([user["email"], user["id"]]);
+    });
+    this.unapprovedUserDataTable = {
+      headerRow: userHeaderRow,
+      dataRows: userDataRow
+    };
+  }
+
+  getUnapprovedUsersFromDB() {
+    this.unapprovedUsersQuery = this.apollo.watchQuery<any>({
+      query: gql`
+        query {
+          users(where: { is_approved: { _eq: false } }) {
+            is_admin
+            id
+            email
+            is_approved
+
+            organizationByOrganizationId {
+              name
+            }
+          }
+        }
+      `,
+
+      pollInterval: 1000,
+
+      fetchPolicy: "network-only"
+    });
+
+    this.unapprovedUsersSubscription = this.unapprovedUsersQuery.valueChanges.subscribe(
+      ({ data }) => {
+        if (data.users.length > 0) {
+          this.allUnapprovedUsers = {};
+          data.users.map(user => {
+            if (user.id !== this.authService.currentUserValue.id) {
+              if (user.id) {
+                this.allUnapprovedUsers[user.id] = {
+                  id: user.id,
+                  name: user.name,
+                  is_admin: user.is_admin,
+                  email: user.email
+                };
+              }
+              if (user.organizationByOrganizationId) {
+                this.allUnapprovedUsers[user.id].organization =
+                  user.organizationByOrganizationId.name;
+              }
+            }
+          });
+          console.log(this.allUnapprovedUsers);
+
+          this.generateUnapprovedUserTable(this.allUnapprovedUsers);
+        } else {
+          this.allUnapprovedUsers = {};
+        }
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 
   getUsersFromDB() {
     this.usersQuery = this.apollo.watchQuery<any>({
       query: gql`
         query {
-          users {
+          users(where: { is_approved: { _eq: true } }) {
             is_admin
             id
             name
+            is_approved
 
             organizationByOrganizationId {
               name
@@ -78,21 +160,23 @@ export class AdminViewComponent implements OnInit, OnDestroy {
           data.users.map(user => {
             if (user.id !== this.authService.currentUserValue.id) {
               if (user.id && user.name) {
-                this.allUsers[user.id] = {
+                this.allApprovedUsers[user.id] = {
                   id: user.id,
                   name: user.name,
                   is_admin: user.is_admin,
-                  organization: "Nil"
+                  organization: "Nil",
+                  is_approved: user.is_approved
                 };
               }
               if (user.organizationByOrganizationId) {
-                this.allUsers[user.id].organization =
+                this.allApprovedUsers[user.id].organization =
                   user.organizationByOrganizationId.name;
               }
             }
           });
+          console.log(this.allApprovedUsers, "all approved");
 
-          this.generateUserTable(this.allUsers);
+          this.generateUserTable(this.allApprovedUsers);
         }
       },
       error => {
@@ -155,11 +239,16 @@ export class AdminViewComponent implements OnInit, OnDestroy {
 
   inviteUser() {
     console.log(this.inviteeEmail);
+    if (!this.userInviteForm.valid) {
+      return;
+    }
+    let email = this.userInviteForm.value.email;
+    console.log(email, "email");
     this.http
       .post(
         "https://invite-flow-microservice-test.dev.jaagalabs.com/invite_user",
         {
-          email: this.inviteeEmail,
+          email: email,
           is_admin: this.authService.currentUserValue["is_admin"]
         }
       )
@@ -172,6 +261,57 @@ export class AdminViewComponent implements OnInit, OnDestroy {
             timer: 1500,
             showConfirmButton: false
           }).catch(swal.noop);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+  }
+
+  approveUser(event, user) {
+    const userId = user[1];
+    console.log(user, "user id");
+    this.apollo
+      .mutate({
+        mutation: gql`
+          mutation update_users{
+            update_users(
+              where: {id: {_eq: ${userId}}},
+              _set: {
+               is_approved:true
+              }
+            ) {
+              affected_rows
+              returning {
+                id
+                is_admin
+                email
+                name
+                is_approved
+                
+              }
+            }
+            
+          }
+        `
+      })
+      .pipe(take(1))
+      .subscribe(
+        data => {
+          let user = data.data.update_users.returning[0];
+          console.log(user, "data");
+          // this.getUnapprovedUsersFromDB();
+
+          if (user.is_approved) {
+            swal({
+              type: "success",
+              title: `${user.email} is approved.`,
+              timer: 1500,
+              showConfirmButton: false
+            }).catch(swal.noop);
+          }
+
+          // console.log(data);
         },
         error => {
           console.log(error);
